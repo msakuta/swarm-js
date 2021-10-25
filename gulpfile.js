152d81c3ecdb30f8556d9824d9e23142766cada8
msakuta <masahiro.sakuta@gmail.com>
2021-10-25 21:56:38 +0900

    Fix gulpfile to watch index.html

--- gulpfile.js
+++ gulpfile.js
@@ -65,8 +65,8 @@ gulp.task('copy-html', function (cb) {
     return gulp.src('./static/index.html').pipe(gulp.dest('./build/'));
 });
 gulp.task('build', gulp.parallel(['compile', 'copy-html']));
-gulp.task('watch', function () {
-    return watch();
-});
+gulp.task('watch', () => compile(true, () => {}));
+
+gulp.watch(['./static/index.html'], gulp.series('copy-html'));
 
 gulp.task('default', gulp.series('watch'));
